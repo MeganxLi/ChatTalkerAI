6233097683866de365ea89820399e54f18a1dc46
Mei Yi <ant19950601@gmail.com>
2022-06-27 21:59:44 +0800

    :truck: fix url

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,7 @@ nav =
    ' <nav class="nav">\
     <div class="container">\
         <a class="logo" href="./"><img src="./img/logo.png" /></a>\
-        <a class="pricing-link" href="/pricing.html">方案費用</a>\
+        <a class="pricing-link" href="pricing.html">方案費用</a>\
     </div>\
     </nav>';
 document.write(nav);
